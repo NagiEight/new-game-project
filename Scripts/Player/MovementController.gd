e6e1ce32f7d3a62b450bc10877062db937584fa0
Zizel <danbua999@gmail.com>
2025-07-17 12:58:55 +0700

    ye

--- Scripts/Player/MovementController.gd
+++ Scripts/Player/MovementController.gd
@@ -1,19 +1,32 @@
 extends CharacterBody2D
 
-var speed = 500
-var gravity = 400
+const SPEED = 500
+const GRAVITY = 1200
+const JUMP_FORCE = -500
+const JUMP_HOLD_TIME = .3
+const EXTRA_GRAVITY = 2000
 
-func _process(delta: float) -> void:
-	pass
+var jump_time = 0.0
+var is_jumping = false
 
 func _physics_process(delta):
-	velocity.y += gravity * delta  # Apply gravity
+	var MoveDirection = Input.get_action_strength("MoveRight") - Input.get_action_strength("MoveLeft")
+	velocity.x = MoveDirection * SPEED
+	
+	if not is_on_floor():
+		if is_jumping and Input.is_action_pressed("Jump") and jump_time < JUMP_HOLD_TIME:
+			jump_time += delta  # Light gravity while holding jump
+		else:
+			velocity.y += EXTRA_GRAVITY * delta  # Fast fall
+			is_jumping = false
+	else:
+		velocity.y = 0
+		jump_time = 0
+		is_jumping = false
 
-	var input = Vector2(
-		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
-		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	).normalized()
-
-	velocity.x = input.x * speed
-
+	# Jump
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		velocity.y = JUMP_FORCE
+		is_jumping = true
+	
 	move_and_slide()
